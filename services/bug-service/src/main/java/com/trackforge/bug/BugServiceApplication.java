46ca03e14f4a83d257e914aabd29c48fd5ee5f5d
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-09-12 16:11:55 +0530

    feat(bug): initialize bug service

--- services/bug-service/src/main/java/com/trackforge/bug/BugServiceApplication.java
+++ services/bug-service/src/main/java/com/trackforge/bug/BugServiceApplication.java
@@ -1,4 +1,15 @@
 package com.trackforge.bug;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class BugServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BugServiceApplication.class, args);
+    }
 }
