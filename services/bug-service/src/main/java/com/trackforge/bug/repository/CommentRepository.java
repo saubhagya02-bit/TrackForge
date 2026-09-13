13243f3ac1663ffb8d9684115a30d5913896a98f
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-09-13 15:52:37 +0530

    feat(bug): add comment repository

--- services/bug-service/src/main/java/com/trackforge/bug/repository/CommentRepository.java
+++ services/bug-service/src/main/java/com/trackforge/bug/repository/CommentRepository.java
@@ -1,4 +1,17 @@
 package com.trackforge.bug.repository;
 
-public class CommentRepository {
+import com.trackforge.bug.entity.ActivityLog;
+import com.trackforge.bug.entity.Attachment;
+import com.trackforge.bug.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findByBugIdOrderByCreatedAtAsc(UUID bugId);
+    long countByBugId(UUID bugId);
 }
+
